c92bd6857cb13e2f4cb15254448e9ebe651b9512
汤启民 <tangiming@foxmail.com>
2021-12-11 17:12:38 +0800

    feat: 源码搬运

---
+++ gee/gee.go
@@ -0,0 +1,44 @@
+/*
+ * @Author: tangqimin
+ * @Date: 2021-11-18 15:26:48
+ * @Description:
+ * @LastEditTime: 2021-11-18 17:22:31
+ * @LastEditors: tangqimin
+ * @FilePath: \Gee\gee\gee.go
+ */
+package gee
+
+import (
+	"net/http"
+)
+
+type HandlerFunc func(*Context)
+
+type Engine struct {
+	router *router
+}
+
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
+}
